c9e71e63dc3bb909c38b51263cc62a405fed94c9
ohsaeuso <ohsaeuso@gmail.com>
2026-06-17 02:35:09 +0900

    event pub test  - wip

--- app/src/main/kotlin/org/example/app/event/UserAccessedEvent.kt
+++ app/src/main/kotlin/org/example/app/event/UserAccessedEvent.kt
@@ -1,5 +1,8 @@
 package org.example.app.event
 
+import java.time.LocalDateTime
+
 data class UserAccessedEvent(
     val username: String,
+    val timestamp: LocalDateTime = LocalDateTime.now()
 )
--- app/src/test/kotlin/org/example/app/service/UserAccessedEventListenerModuleTest.kt
+++ app/src/test/kotlin/org/example/app/service/UserAccessedEventListenerModuleTest.kt
@@ -3,20 +3,31 @@ package org.example.app.service
 import org.assertj.core.api.Assertions.assertThat
 import org.example.app.event.UserAccessedEvent
 import org.junit.jupiter.api.Test
-import org.springframework.context.ApplicationEventPublisher
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.modulith.test.ApplicationModuleTest
-import org.springframework.modulith.test.PublishedEvents
+import org.springframework.modulith.test.Scenario
+import java.util.function.Consumer
+
 
 @ApplicationModuleTest
 class UserAccessedEventListenerModuleTest {
 
-    @Test
-    fun handle_givenUserAccessedEvent_thenAuditServiceLogsAccess(
-        publisher: ApplicationEventPublisher,
-        events: PublishedEvents,
-    ) {
-        publisher.publishEvent(UserAccessedEvent("alice"))
+    @Autowired
+    private val userAccessService: UserAccessService? = null
 
-        assertThat(events.ofType(UserAccessedEvent::class.java)).hasSize(1)
+    @Autowired
+    private val userAccessAuditService: UserAccessAuditService? = null
+
+    // https://www.baeldung.com/spring-test-application-events
+
+    @Test
+    fun whenRecordAccess_thenUserAccessAuditServiceAudit(scenario: Scenario) {
+        scenario.stimulate(Runnable { userAccessService?.recordAccess("customer-1") })
+            .andWaitForEventOfType(UserAccessedEvent::class.java)
+            .toArriveAndVerify(Consumer { evt: UserAccessedEvent? ->
+                assertThat(evt)
+                    .hasFieldOrPropertyWithValue("username", "customer-1")
+                    .hasFieldOrProperty("timestamp")
+            })
     }
 }
